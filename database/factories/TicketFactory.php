43110be8ae1dbbf479c617ed30f2ed3d4818d91c
oleksandr.o <oleksandr.o@toolza.io>
2026-01-22 15:42:54 +0200

    added tests

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Ticket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
+ */
+class TicketFactory extends Factory
+{
+    protected $model = Ticket::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'description' => $this->faker->paragraph(),
+            'status' => 'Open',
+            'category' => null,
+            'sentiment' => null,
+            'suggested_reply' => null,
+        ];
+    }
+}
